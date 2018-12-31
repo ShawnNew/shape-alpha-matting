f56a85c2de3ecea80c11a37c32375e0a77664cda
ShawnNew <ncxinhanzhong@gmail.com>
2018-12-31 21:57:50 +0000

    check network passed until unpooling

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,30 +1,48 @@
 import h5py
 import os
+import cv2
 import numpy as np
 import caffe
 import pdb
-# from __future__ import print_function
 DATASET = "/home/dl/Downloads/datasets/alphamatting/low-res/"
 DIR = "../dataset"
-RAW_IMAGE_SIZE = 256
+RAW_IMAGE_SIZE = 224
+h5_filename = 'alphamatting.h5'
+text_filename = 'alphamatting.txt'
+shuffle = True
 
+def write_files(data, tri_map, label):    
+    text_filedir = os.path.join(DIR, text_filename)
+    h5_filedir = os.path.join(DIR, h5_filename)
+    hdf_file = h5py.File(h5_filedir, 'w')
+    datum_shape = data[0].shape
+    chunks = (1, datum_shape[0], datum_shape[1], datum_shape[2])
+    hdf_file.create_dataset('data', dtype=np.float, data=data, chunks=chunks)
+    hdf_file.create_dataset('tri-map', dtype=np.float, data=tri_map, chunks=chunks)
+    hdf_file.create_dataset('label', dtype=np.float, data=label, chunks=chunks)
+    hdf_file.flush()
+    hdf_file.close()
+    # with h5py.File(h5_filedir, 'w') as f:
+    #     f.create_dataset('data', data=data)
+    #     f.create_dataset('tri-map', data=tri_map)
+    #     f.create_dataset('label', data=label)
 
-def write_files(data, tri_map, label):
-    h5_filename = os.path.join(DIR, 'alphamatting.h5')
-    text_filename = os.path.join(DIR, 'alphamatting.txt')
+    with open(text_filedir, 'w') as f:
+        f.write(h5_filedir)
 
-    with h5py.File(h5_filename, 'w') as f:
-        f.create_dataset('data', data=data)
-        f.create_dataset('tri-map', data=tri_map)
-        f.create_dataset('label', data=label)
-
-    with open(text_filename, 'w') as f:
-        # print(h5_filename, file=f)
-        f.write(h5_filename.replace('../dataset/', ''))
-
+def caffe_transform(img):
+    img = cv2.resize(img, \
+                    (RAW_IMAGE_SIZE, RAW_IMAGE_SIZE),\
+                    interpolation=cv2.INTER_CUBIC
+                )
+    img = np.transpose(img, (2, 0, 1))
+    #TODO: normalization to be done here
+    return img / 255.
 
 if __name__ == '__main__':
     fn = os.path.join(DATASET, 'train.txt')
+    print "Dataset directiry is:", DATASET
+    img_count = 1
     with open(fn, 'r') as f:
         data_lines = f.readlines()
         data = np.zeros(
@@ -39,27 +57,31 @@ if __name__ == '__main__':
             (len(data_lines), 3, RAW_IMAGE_SIZE, RAW_IMAGE_SIZE), \
             dtype='f4'
         )
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         for i, l in enumerate(data_lines):
-            l = l.rstrip()
-            l = l.replace('./', DATASET)
-            items = l.split(' ')
-            raw_img = caffe.io.load_image(items[0])
-            #TODO: img is not resize correctly here
-            raw_img = caffe.io.resize(
-                raw_img, (3, RAW_IMAGE_SIZE, RAW_IMAGE_SIZE)
-            )
-            
-            raw_tri_map = caffe.io.load_image(items[1])
-            raw_tri_map = caffe.io.resize(
-                raw_tri_map, (3, RAW_IMAGE_SIZE, RAW_IMAGE_SIZE)
-            )
-            raw_label = caffe.io.load_image(items[2])
-            raw_label = caffe.io.resize(
-                raw_label, (3, RAW_IMAGE_SIZE, RAW_IMAGE_SIZE)
-            )
-            data[i] = raw_img
-            tri_map[i] = raw_tri_map
-            label = raw_label
-            pdb.set_trace()
+            print "Preprocessed %d images"% img_count
+            # parse data directory
+            items = l.rstrip().replace('./', DATASET).split(' ')
+            # load raw dataset images
+            #TODO: the channel order may not align with vgg-16 pre-trained model
+            # here in opencv, the channel order is BGR
+            raw_img = cv2.imread(items[0]).astype(np.float64)
+            raw_tri_map = cv2.imread(items[1]).astype(np.float64)
+            raw_label = cv2.imread(items[2]).astype(np.float64)
+            # pre-process
+            data[i] = caffe_transform(raw_img)
+            tri_map[i] = caffe_transform(raw_tri_map)
+            label[i] = caffe_transform(raw_label)
+            img_count += 1
+        print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        print "All %d images are been processed, write to hdf5 file(%s).\n"% \
+            (img_count-1, h5_filename)
         # pdb.set_trace()
+
+        # shuffle
+        if shuffle:
+            perm = np.random.permutation(len(data_lines))
+            data = data[perm]
+            tri_map = tri_map[perm]
+            label = label[perm]
         write_files(data, tri_map, label)
