bce54619d9400f9e741d9105c4b168efee424ac0
ShawnNew <ncxinhanzhong@gmail.com>
2019-01-10 23:56:00 +0000

    at least pass the test

--- scripts/alphaLoss.py
+++ scripts/alphaLoss.py
@@ -1,6 +1,9 @@
 import caffe
 import numpy as np
 import glog
+import cv2
+import pdb
+from PIL import Image
 
 class AlphaMattingLossLayer(caffe.Layer):
     """
@@ -50,10 +53,10 @@ class AlphaMattingLossLayer(caffe.Layer):
         alpha = bottom[1].data[:, 4, :, :]
         fg = bottom[1].data[:, 5:8, :, :]
         bg = bottom[1].data[:, 8:11, :, :]
-        top[0].data[...] = self.overall_loss(pred, mask, alpha, color_img, fg, bg)
-        pred = np.reshape(pred, (-1, 1, self.shape[0], self.shape[1]))
-        alpha = np.reshape(alpha, (-1, 1, self.shape[0], self.shape[1]))
-        self.diff[...] = pred - alpha
+        top[0].data[...] = self.overall_loss(pred, mask, alpha, color_img, fg, bg)        
+        # pred = np.reshape(pred, (-1, 1, self.shape[0], self.shape[1]))
+        # alpha = np.reshape(alpha, (-1, 1, self.shape[0], self.shape[1]))
+        # self.diff[...] = pred - alpha
 
     def backward(self, top, progagate_down, bottom):
         # calculate backpropogation gradient here.
@@ -67,33 +70,72 @@ class AlphaMattingLossLayer(caffe.Layer):
         #    bottom[i].diff[...] = sign * self.diff \
         #                            / top[0].data \
                                     # / bottom[i].num
-        for i in range(len(bottom[0].data)):
-            bottom[0].diff[i][0] = np.dot(self.diff[i][0] / top[0].data, bottom[0].data[i][0])
-        #bottom[0].diff[...] = (self.diff / top[0].data) * bottom[0].data 
+        # for i in range(len(bottom[0].data)):
+        #     bottom[0].diff[i][0] = np.dot(self.diff[i][0] / top[0].data, bottom[0].data[i][0])
+        #bottom[0].diff[...] = (self.diff / top[0].data) * bottom[0].data
+        self.diff_alpha = np.reshape(self.diff_alpha, (-1, 1, self.shape[0], self.shape[1])) 
+        self.sqrt_alpha = np.reshape(self.sqrt_alpha, (-1, 1, self.shape[0], self.shape[1]))
+        self.diff_comp = np.reshape(self.diff_alpha, (-1, 1, self.shape[0], self.shape[1])) 
+        self.sqrt_comp = np.reshape(self.sqrt_comp, (-1, 1, self.shape[0], self.shape[1]))
+        # print self.diff_comp.shape
+        # print self.sqrt_comp.shape
+        # print self.diff_alpha.shape
+        # print self.sqrt_alpha.shape
+        # exit()         
+        temp = self.w_l * self.diff_alpha / self.sqrt_alpha + \
+                                (1 - self.w_l) * self.diff_comp / self.sqrt_comp
+        # print bottom[0].diff.shape
+        # print type(bottom[0].diff[0][0][0][0])
+        # print type(temp[0][0][0][0])
+        # print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        # print bottom[0].diff
+        # print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        # print temp
+        bottom[0].diff[...] = temp
        
     def alpha_prediction_loss(self, mask, pred, alpha):
         # calculate alpha_prediction_loss here
-        diff = pred - alpha
-        diff = diff * mask       # element-wise multiply
+        diff = pred - alpha  # 4*224*224
+        # pdb.set_trace()
+        # print diff
+        # print "=============================================="
+        # self.diff_alpha = np.average(diff, axis=0)            
+        diff = diff * mask       # element-wise multiply        
+        self.diff_alpha = diff
         num_pixels = np.sum(mask)
-        return np.sum(np.sqrt(np.square(diff) + self.epsilon**2)) \
-        / (num_pixels + self.epsilon)
-        #/ len(pred) # divid the batch size and the unknown region
+        self.sqrt_alpha = np.sqrt(np.square(diff) + self.epsilon**2)
+        # self.sqrt_alpha = np.average(self.sqrt_alpha, axis = 0)
+        return np.sum(self.sqrt_alpha) \
+        / (num_pixels + self.epsilon) \
+        # / len(pred) # divid the batch size and the unknown region
 
     def compositional_loss(self, pred, mask, color_img, fg, bg):
         # calculate compositional_loss here
         mask = np.reshape(mask, (-1, 1, self.shape[0], self.shape[1]))
         pred = np.reshape(pred, (-1, 1, self.shape[0], self.shape[1]))
         color_pred = pred * fg + (1.0 - pred) * bg    # element-wise multiply to get color image
-        diff = color_pred - color_img
+        diff = color_pred - color_img   # 3 channels
+        # self.diff_comp = np.average(np.average(diff, axis=0), axis=0)        
         diff = diff * mask
+        self.diff_comp = np.average(diff, axis=1)
         num_pixels = np.sum(mask)
-        return np.sum(np.sqrt(np.square(diff) + self.epsilon**2)) \
-        / (num_pixels + self.epsilon)
-        #/ len(pred) # divide the batch size and the unknown region
+        # self.sqrt_comp = np.average(np.average(np.sqrt(np.square(diff) + self.epsilon**2), \
+        #                              axis=0), axis=0)
+        self.sqrt_comp = np.average(np.sqrt(np.square(diff) + self.epsilon**2), \
+                                     axis=1)  # rgb average (4*1*224*224)
+        return np.sum(self.sqrt_comp) \
+        / (num_pixels + self.epsilon) \
+        # / len(pred) # divide the batch size and the unknown region
 
-    def overall_loss(self, pred, mask, alpha, color_img, fg, bg):
-        # average the above two losses
-        return self.w_l * self.alpha_prediction_loss(mask, pred, alpha) + \
-                (1 - self.w_l) * self.compositional_loss(pred, mask, color_img, fg, bg)
+
+    def overall_loss(self, pred, mask, alpha, color_img, fg, bg):                
+        # average the above two losses        
+        mask[mask == 0.] *= 0.
+        mask[mask == 1.] *= 0.
+        mask[mask != 0.] = 1.
+        alpha_loss = self.alpha_prediction_loss(mask, pred, alpha)
+        comp_loss = self.compositional_loss(pred, mask, color_img, fg, bg)
+        # glog.warn("alpha loss: " + str(alpha_loss) + " comp loss: " + str(comp_loss))
+        return self.w_l * alpha_loss + \
+                (1 - self.w_l) * comp_loss
         
